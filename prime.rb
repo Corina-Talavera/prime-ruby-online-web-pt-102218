af42439d4460b41b484717dd59aa5d7028f0c84f
Corina Talavera <corina.talavera@gmail.com>
2018-11-03 16:50:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,9 @@
 require_relative './spec_helper'
 require_relative '../prime.rb'
 
+def "prime?"
+  
+
 describe "prime?" do
   it 'returns true for prime numbers' do
     expect(prime?(2)).to be(true)
