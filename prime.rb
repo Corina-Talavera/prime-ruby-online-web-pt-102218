a9a6352385e6b522332de1e55ee9dca389f088e8
Corina Talavera <corina.talavera@gmail.com>
2018-11-03 16:57:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,16 @@
 # Add  code here!
-require_relative './spec_helper'
-require_relative '../prime.rb'
-
 def prime_true
-  prime?(2) #=> true
+  prime?(2) #=> TRUE
   prime?(3) #=> true
   prime?(11) #=> true
   prime?(105557) #=> true
 end
 
-  it 'returns false for non-prime numbers' do
-    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
-    expect(prime?(0)).to be(false)
-    expect(prime?(1)).to be(false)
-    expect(prime?(4)).to be(false)
-    expect(prime?(40)).to be(false)
-    expect(prime?(1763)).to be(false)
-    expect(prime?(101013)).to be(false)
+  def prime_false
+    prime?(0) #=> FALSE
+    prime?(1) #=> FALSE
+    prime?(4) #=> FALSE
+    prime?(40) #=> FALSE
+    prime?(1763) #=> FALSE
+    prime?(101013) #=> FALSE
   end
-end
